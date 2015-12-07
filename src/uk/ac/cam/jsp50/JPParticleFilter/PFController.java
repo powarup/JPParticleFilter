808889587edb68dc1c7e68fb75527a0ea70ed160
Jovan Powar <jovan@powarup.com>
2015-12-07 15:19:29 +0000

    Controller now uses singleton SVG, flexible init

--- src/uk/ac/cam/jsp50/JPParticleFilter/PFController.java
+++ src/uk/ac/cam/jsp50/JPParticleFilter/PFController.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import uk.ac.cam.jsp50.JPParticleFilter.PFRandom.PFRandomInstanceAlreadyExistsException;
 import uk.ac.cam.jsp50.JPParticleFilter.ParticleStore.ParticleManager;
 import uk.ac.cam.jsp50.JPParticleFilter.ParticleStore.ParticleNotFoundException;
+import uk.ac.cam.jsp50.JPParticleFilter.StepVectorGenerator.StepVectorGeneratorInstanceAlreadyExistsException;
 
 public class PFController {
 
@@ -15,7 +16,7 @@ public class PFController {
 	
 	public static FloorPlan floorPlan;
 	public static ParticleStore particleStore;
-	public static final int initialParticleNo = 30;
+	public static final int maxParticleNo = 300;
 	public static final int degeneracyLimit = 30;
 	public static int activeParticles = 0;
 	
@@ -23,22 +24,27 @@ public class PFController {
 	private static Scanner scan;
 	
 	public static void init(ParticleStoreType type) {
+		initWithParticleNo(type,maxParticleNo);
+	}
+	
+	public static void initWithParticleNo(ParticleStoreType type, int particleNo) {
 		switch (type) {
 		case OBJECT:
 			particleStore = new ObjectParticleStore();
 			break;
 		case ARRAY:
-			particleStore = new ArrayParticleStore(initialParticleNo);
+			particleStore = new ArrayParticleStore(particleNo);
 			break;
 		default:
-			particleStore = new ArrayParticleStore(initialParticleNo);
+			particleStore = new ArrayParticleStore(particleNo);
 			break;
 		}
 		double x,y;
 		PFRandom randomGenerator = PFRandom.getInstance();
 		
-		double weight = 1.0/initialParticleNo;
-		while (activeParticles < initialParticleNo) {
+		double weight = 1.0/particleNo;
+		while (activeParticles < particleNo) {
+			System.out.println("Making " + particleNo + " particles. Current #active particles: " + activeParticles);
 			x = randomGenerator.nextDouble() * floorPlan.maxX;
 			y = randomGenerator.nextDouble() * floorPlan.maxY;
 						
@@ -47,7 +53,7 @@ public class PFController {
 				activeParticles++;
 			}
 		}
-		System.out.println("initialised with " + initialParticleNo + " particles");
+		System.out.println("initialised with " + particleNo + " particles");
 	}
 	
 	public static void propagate(StepVector s) {
@@ -98,9 +104,9 @@ public class PFController {
 			totalWeight += particleManager.getWeight();
 		}
 		
-		double[] randomN = new double[initialParticleNo];
+		double[] randomN = new double[maxParticleNo];
 		PFRandom randomiser = PFRandom.getInstance();
-		for (int i = 0; i < initialParticleNo; i++) {
+		for (int i = 0; i < maxParticleNo; i++) {
 			randomN[i] = randomiser.nextDouble();
 		}
 		
@@ -118,7 +124,7 @@ public class PFController {
 		// if weight less than current total weight seen, generate new particle
 		// else get new particle, update total weight, and try again
 		
-		for (int j = 0; j < initialParticleNo; j++) {
+		for (int j = 0; j < maxParticleNo; j++) {
 			while (randomN[j] > weightTotal) {
 				try {
 					particleManager.nextActiveParticle();
@@ -165,7 +171,7 @@ public class PFController {
 		// argument taken is location of floor plan csv, initialise floorPlan data structure
 		String floorPlanPath = args[0];
 		floorPlan = new FloorPlan(floorPlanPath);
-
+		
 		try {
 			PFRandom.startInstanceWithFile("1MRandoms.txt");
 		} catch (PFRandomInstanceAlreadyExistsException e1) {
@@ -174,11 +180,18 @@ public class PFController {
 		
 		showMemory();
 		startTime = System.currentTimeMillis();
-		init(ParticleStoreType.OBJECT);
+		initWithParticleNo(ParticleStoreType.OBJECT,1);
 		endTime = System.currentTimeMillis();
 		System.out.println("init took " + (endTime - startTime) + "ms");
 		showMemory();
-		StepVectorGenerator stepGen = new StepVectorGenerator();
+		
+		StepVectorGenerator stepGen;
+		try {
+			stepGen = StepVectorGenerator.startGeneratorFromFile("1KRight.csv");
+		} catch (StepVectorGeneratorInstanceAlreadyExistsException e1) {
+			System.out.println("could not instantiate StepVectorGenerator from file");
+			stepGen = StepVectorGenerator.getInstance();
+		}
 		
 		StepVector nextStep;
 		scan = new Scanner(System.in);
