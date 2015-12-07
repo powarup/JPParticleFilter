6f65903776558ac4872d088884517a4545369ab0
Jovan Powar <jovan@powarup.com>
2015-12-07 12:59:05 +0000

    Added caching to PFRandom

--- src/uk/ac/cam/jsp50/JPParticleFilter/PFRandom.java
+++ src/uk/ac/cam/jsp50/JPParticleFilter/PFRandom.java
@@ -3,6 +3,7 @@ package uk.ac.cam.jsp50.JPParticleFilter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 
@@ -20,6 +21,9 @@ public class PFRandom {
 	private static boolean usingFile = false;
 	private static Random randomiser;
 	public static Queue<Double> doubles;
+	public static int cacheSize = 1000;
+	public static int cacheLimit = 10;
+	private BufferedReader br;
 	
 	private static PFRandom singleton;
 	
@@ -29,16 +33,29 @@ public class PFRandom {
 	
 	protected PFRandom(String filePath) {
 		usingFile = true;
-		System.out.println("loading random numbers from " + filePath);
-		BufferedReader br;
-		String line = "";
+		doubles = new LinkedList<Double>();
+		System.out.println("PFRandom:: loading random numbers from " + filePath);
 		try {
 			br = new BufferedReader(new FileReader(filePath));
-			while ((line = br.readLine()) != null) {
-				doubles.add(Double.parseDouble(line));
-			}
+			loadCache();
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
+		}
+	}
+	
+	private void loadCache() {
+		System.out.println("PFRandom:: filling cache");
+		String line = "";
+		try {
+			while ((line = br.readLine()) != null && doubles.size() < cacheSize) {
+				doubles.add(Double.parseDouble(line));
+			}
+			if (line == null) {
+				System.err.println("PFRandom:: run out of random numbers");
+			}
+		} catch (NumberFormatException | IOException e) {
+			System.out.println("PFRandom:: exception");
+			System.err.println(e.getMessage());
 		}
 	}
 	
@@ -46,7 +63,7 @@ public class PFRandom {
 		randomiser = new Random(seed);
 	}
 
-	public PFRandom getInstance() {
+	public static PFRandom getInstance() {
 		if (singleton == null) {
 			singleton = new PFRandom();
 		}
@@ -68,11 +85,31 @@ public class PFRandom {
 	}
 	
 	public double nextDouble() {
+		if (doubles.size() <= cacheLimit) loadCache();
 		if (!usingFile) {
 			return randomiser.nextDouble();
 		} else {
 			return doubles.poll();
 		}
 	}
-	
+	private double nextNextGaussian;
+	private boolean haveNextNextGaussian = false;
+
+	public double nextGaussian() {
+		if (haveNextNextGaussian) {
+			haveNextNextGaussian = false;
+			return nextNextGaussian;
+		} else {
+			double v1, v2, s;
+			do {
+				v1 = 2 * nextDouble() - 1;   // between -1.0 and 1.0
+				v2 = 2 * nextDouble() - 1;   // between -1.0 and 1.0
+				s = v1 * v1 + v2 * v2;
+			} while (s >= 1 || s == 0);
+			double multiplier = StrictMath.sqrt(-2 * StrictMath.log(s)/s);
+			nextNextGaussian = v2 * multiplier;
+			haveNextNextGaussian = true;
+			return v1 * multiplier;
+		}
+	}
 }
