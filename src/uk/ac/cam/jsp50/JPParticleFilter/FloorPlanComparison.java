fc32d627c155f9622b4327d8a98f58fcb5dcd42b
Jovan Powar <jovan@powarup.com>
2016-03-31 15:15:49 +0100

    Overhauled basic comparisons

--- src/uk/ac/cam/jsp50/JPParticleFilter/FloorPlanComparison.java
+++ src/uk/ac/cam/jsp50/JPParticleFilter/FloorPlanComparison.java
@@ -10,32 +10,47 @@ public class FloorPlanComparison extends PFComparison {
 		boolean visualise = false;
 		InputStream floorPlanStream;
 		int nGenerations = 100;
-		String prefix = "comparisonResults/floor plan comparison - " + testName + " - " + initialParticleNo + " - ";
-
+		String prefix = "comparisonResults/fp/floor plan comparison - " + maxParticleNo + " particles - " + testName + " - ";
+		FloorPlanType floorPlanType;
 
 		// NAIVE runs
+		floorPlanType = FloorPlanType.NAIVE;
 		// run for memory
 		floorPlanStream = new FileInputStream(floorPlanPath);
-		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, FloorPlanType.NAIVE, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, true, false, false, false);
+		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, floorPlanType, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, true, false, false, false);
 		runAndExport(nGenerations, prefix + "naive memory.csv");
 		System.out.println("DONE. \n");
 		// run for time
 		floorPlanStream = new FileInputStream(floorPlanPath);
-		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, FloorPlanType.NAIVE, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, false, true, false, false);
+		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, floorPlanType, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, false, true, false, false);
 		runAndExport(nGenerations, prefix + "naive time.csv");
 		System.out.println("DONE. \n");
 
 
 		// SIMPLE_BITMAP runs
+		floorPlanType = FloorPlanType.SIMPLE_BITMAP;
 		// run for memory
 		floorPlanStream = new FileInputStream(floorPlanPath);
-		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, FloorPlanType.SIMPLE_BITMAP, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, true, false, false, false);
+		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, floorPlanType, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, true, false, false, false);
 		runAndExport(nGenerations, prefix + "simplebitmap memory.csv");
 		System.out.println("DONE. \n");
 		// run for time
 		floorPlanStream = new FileInputStream(floorPlanPath);
-		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, FloorPlanType.SIMPLE_BITMAP, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, false, true, false, false);
+		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, floorPlanType, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, false, true, false, false);
 		runAndExport(nGenerations, prefix + "simplebitmap time.csv");
+		System.out.println("DONE. \n");
+
+		// COMPLEX_BITMAP runs
+		floorPlanType = FloorPlanType.COMPLEX_BITMAP;
+		// run for memory
+		floorPlanStream = new FileInputStream(floorPlanPath);
+		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, floorPlanType, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, true, false, false, false);
+		runAndExport(nGenerations, prefix + "complexbitmap memory.csv");
+		System.out.println("DONE. \n");
+		// run for time
+		floorPlanStream = new FileInputStream(floorPlanPath);
+		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, floorPlanType, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, false, true, false, false);
+		runAndExport(nGenerations, prefix + "complexbitmap time.csv");
 		System.out.println("DONE. \n");
 	}
 
@@ -45,108 +60,107 @@ public class FloorPlanComparison extends PFComparison {
 		initialParticleNo = 10000;
 		maxParticleNo = 10000;
 		degeneracyLimit = 5000;
-		
+
 		String randomFilePath, floorPlanPath;
 		String stepVectorFilePath = null;
 
 		// one point
 		floorPlanPath = "comparisonData/onePoint.csv";
-		
+
 		randomFilePath = "comparisonData/floorplanrandoms1.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "one point 1");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "1 points 1");
 
 		randomFilePath = "comparisonData/floorplanrandoms2.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "one point 2");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "1 points 2");
 
 		// ten points
 		floorPlanPath = "comparisonData/tenPoints.csv";
-		
+
 		randomFilePath = "comparisonData/floorplanrandoms1.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "ten points 1");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "10 points 1");
 
 		randomFilePath = "comparisonData/floorplanrandoms2.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "ten points 2");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "10 points 2");
 
 		// fifty points
 		floorPlanPath = "comparisonData/fiftyPoints.csv";
-		
+
 		randomFilePath = "comparisonData/floorplanrandoms1.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "fifty points 1");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "50 points 1");
 
 		randomFilePath = "comparisonData/floorplanrandoms2.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "fifty points 2");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "50 points 2");
 
 		// hundred points
 		floorPlanPath = "comparisonData/hundredpoints.csv";
-		
+
 		randomFilePath = "comparisonData/floorplanrandoms1.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "hundred points 1");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "100 points 1");
 
 		randomFilePath = "comparisonData/floorplanrandoms2.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "hundred points 2");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "100 points 2");
 
 		// five hundred points
 		floorPlanPath = "comparisonData/fivehundredpoints.csv";
-		
+
 		randomFilePath = "comparisonData/floorplanrandoms1.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "five hundred points 1");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "500 points 1");
 
 		randomFilePath = "comparisonData/floorplanrandoms2.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "five hundred points 2");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "500 points 2");
 
 		// thousand points
 		floorPlanPath = "comparisonData/thousandpoints.csv";
-		
+
 		randomFilePath = "comparisonData/floorplanrandoms1.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "thousand points 1");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "1000 points 1");
 
 		randomFilePath = "comparisonData/floorplanrandoms2.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "thousand points 2");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "1000 points 2");
 
 		// five thousand points
 		floorPlanPath = "comparisonData/fivethousandpoints.csv";
-		
+
 		randomFilePath = "comparisonData/floorplanrandoms1.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "five thousand points 1");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "5000 points 1");
 
 		randomFilePath = "comparisonData/floorplanrandoms2.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "five thousand points 2");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "5000 points 2");
 
 
 		// ten thousand points
 		floorPlanPath = "comparisonData/tenthousandpoints.csv";
-		
+
 		randomFilePath = "comparisonData/floorplanrandoms1.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "ten thousand points 1");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "10000 points 1");
 
 		randomFilePath = "comparisonData/floorplanrandoms2.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "ten thousand points 2");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "10000 points 2");
 
 
-		// polygon1 100 particles
-		//naive
-		//bitmap
+		// polygon1
+		floorPlanPath = "polygon1.csv";
 
-		// polygon1 1000 particles
-		//naive
-		//bitmap
+		randomFilePath = "comparisonData/floorplanrandoms1.txt";
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "polygon1 1");
 
-		// polygon1 10000 particles
-		//naive	
-		//bitmap
+		randomFilePath = "comparisonData/floorplanrandoms2.txt";
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "polygon1 2");
 
 
-		// complex 100 particles
-		//naive
-		//bitmap
+		// wgb_second
+		initialParticleNo = 25000;
+		maxParticleNo = 3000;
+		degeneracyLimit = 1000;
+		floorPlanPath = "wgb_second.csv";
 
-		// complex 1000 particles
-		//naive
-		//bitmap
+		randomFilePath = "comparisonData/floorplanrandoms1.txt";
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "wgb_second 1");
 
-		// complex 10000 particles
-		//naive
-		//bitmap
+		randomFilePath = "comparisonData/floorplanrandoms2.txt";
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "wgb_second 2");
+
+
 
 	}
 
--- src/uk/ac/cam/jsp50/JPParticleFilter/ParticleStoreComparison.java
+++ src/uk/ac/cam/jsp50/JPParticleFilter/ParticleStoreComparison.java
@@ -10,21 +10,22 @@ public class ParticleStoreComparison extends PFComparison {
 		boolean visualise = false;
 		InputStream floorPlanStream;
 		int nGenerations = 100;
-		String prefix = "comparisonResults/particle store comparison - " + testName + " - ";
-		
+		String prefix = "comparisonResults/particle store comparison - " + maxParticleNo + " particles - " + testName + " - ";
+		FloorPlanType floorPlanType = FloorPlanType.COMPLEX_BITMAP;
+
 		// object
 		// memory run
 		System.out.println("OBJECT: (memory) " + testName);
 		System.gc();
 		floorPlanStream = new FileInputStream(floorPlanPath);
-		PFController.setupFilter(floorPlanStream, ParticleStoreType.OBJECT, FloorPlanType.NAIVE, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, true, false, false, false);
+		PFController.setupFilter(floorPlanStream, ParticleStoreType.OBJECT, floorPlanType, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, true, false, false, false);
 		runAndExport(nGenerations,prefix + "object memory.csv");
 		System.out.println("DONE. \n");
 		// time run
 		System.out.println("OBJECT: (time) " + testName);
 		System.gc();
 		floorPlanStream = new FileInputStream(floorPlanPath);
-		PFController.setupFilter(floorPlanStream, ParticleStoreType.OBJECT, FloorPlanType.NAIVE, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, false, true, false, false);
+		PFController.setupFilter(floorPlanStream, ParticleStoreType.OBJECT, floorPlanType, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, false, true, false, false);
 		runAndExport(nGenerations,prefix + "object time.csv");
 		System.out.println("DONE. \n");
 
@@ -34,115 +35,92 @@ public class ParticleStoreComparison extends PFComparison {
 		System.out.println("ARRAY: (memory) " + testName);
 		System.gc();
 		floorPlanStream = new FileInputStream(floorPlanPath);
-		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, FloorPlanType.NAIVE, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, true, false, false, false);
+		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, floorPlanType, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, true, false, false, false);
 		runAndExport(100,prefix + "array memory.csv");
 		System.out.println("DONE. \n");
 		// time run
 		System.out.println("ARRAY: (time) " + testName);
 		System.gc();
 		floorPlanStream = new FileInputStream(floorPlanPath);
-		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, FloorPlanType.NAIVE, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, false, true, false, false);
+		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, floorPlanType, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, false, true, false, false);
 		runAndExport(100,prefix + "array time.csv");
 		System.out.println("DONE. \n");
 	}
 
 	public static void main(String[] args) throws IOException {
-//		InputStream floorPlanStream;
 		int initialParticleNo, maxParticleNo, degeneracyLimit;
-//		boolean visualise = false;
 		String randomFilePath;
 		String stepVectorFilePath = null;
 		String floorPlanPath;
-		
-		floorPlanPath = "polygon1.csv";
-		
+
+		floorPlanPath = "comparisonData/onepoint.csv";
+
 
 		// 100 particles - simple
 		initialParticleNo = 100;
 		maxParticleNo = 100;
 		degeneracyLimit = 50;
-		//floorPlanPath = "comparisonData/onePoint.csv";
 		for (int i = 1; i <= 10; i++) {
 			randomFilePath = "comparisonData/randoms/particlerandoms" + i + ".txt";
 			int runNo = i % 10;
 			if (runNo == 0) runNo = 10;
-			runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "100 particles no collisions " + runNo);
+			runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "no collisions complex " + runNo);
 		}
-//		randomFilePath = "comparisonData/particlerandoms1.txt";
-//		// object (memory run)
-//		floorPlanStream = new FileInputStream("polygon1.csv");
-//		PFController.setupFilter(floorPlanStream, ParticleStoreType.OBJECT, FloorPlanType.NAIVE, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, true, false, false, false);
-//		runAndExport(100,"comparisonResults/particleComparison object 100 simple memory.csv");
-//		// array (memory run)
-//		floorPlanStream = new FileInputStream("polygon1.csv");
-//		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, FloorPlanType.NAIVE, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, true, false, false, false);
-//		runAndExport(100,"comparisonResults/particleComparison array 100 simple memory.csv");
-//
-//		// 100 particles - complex
-//		// object
-//
-//		// array
 
 
 		// 1000 particles - simple
 		initialParticleNo = 1000;
 		maxParticleNo = 1000;
 		degeneracyLimit = 500;
-		//floorPlanPath = "comparisonData/onePoint.csv";
 		for (int i = 11; i <= 20; i++) {
 			randomFilePath = "comparisonData/randoms/particlerandoms" + i + ".txt";
 			int runNo = i % 10;
 			if (runNo == 0) runNo = 10;
-			runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "1000 particles no collisions " + runNo);
+			runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "no collisions complex " + runNo);
 		}
-//		// object (memory run)
-//		floorPlanStream = new FileInputStream("polygon1.csv");
-//		PFController.setupFilter(floorPlanStream, ParticleStoreType.OBJECT, FloorPlanType.NAIVE, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, true, false, false, false);
-//		runAndExport(100,"comparisonResults/particleComparison object 1000 simple memory.csv");
-//		// array (memory run)
-//		floorPlanStream = new FileInputStream("polygon1.csv");
-//		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, FloorPlanType.NAIVE, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, visualise, true, false, false, false);
-//		runAndExport(100,"comparisonResults/particleComparison array 1000 simple memory.csv");
-//
-//
-//		// 1000 particles - complex
-//		// object
-//
-//		// array
 
 		// 10000 particles - simple
 		initialParticleNo = 10000;
 		maxParticleNo = 10000;
 		degeneracyLimit = 5000;
-		//floorPlanPath = "comparisonData/onePoint.csv";
 		for (int i = 21; i <= 30; i++) {
 			randomFilePath = "comparisonData/randoms/particlerandoms" + i + ".txt";
 			int runNo = i % 10;
 			if (runNo == 0) runNo = 10;
-			runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "10000 particles no collisions " + runNo);
+			runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "no collisions complex " + runNo);
 		}
-		
-		// 10000 particles - simple
+
+		// 100000 particles - simple
 		initialParticleNo = 100000;
 		maxParticleNo = 100000;
 		degeneracyLimit = 50000;
-		//floorPlanPath = "comparisonData/onePoint.csv";
 		randomFilePath = "comparisonData/bigparticlerandoms1.txt";
-		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "100000 particles no collisions");
-		
-//		FileInputStream floorPlanStream;
-//		randomFilePath = "comparisonData/bigparticlerandoms1.txt";
-//		initialParticleNo = 100000;
-//		maxParticleNo = 100000;
-//		degeneracyLimit = 50000;
-//		// object (memory run)
-//		floorPlanStream = new FileInputStream("polygon1.csv");
-//		PFController.setupFilter(floorPlanStream, ParticleStoreType.OBJECT, FloorPlanType.NAIVE, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, false, true, false, false, false);
-//		runAndExport(100,"comparisonResults/particleComparison object 100000 simple memory.csv");
-//		// array (memory run)
-//		floorPlanStream = new FileInputStream("polygon1.csv");
-//		PFController.setupFilter(floorPlanStream, ParticleStoreType.ARRAY, FloorPlanType.NAIVE, initialParticleNo, maxParticleNo, degeneracyLimit, randomFilePath, stepVectorFilePath, false, true, false, false, false);
-//		runAndExport(100,"comparisonResults/particleComparison array 100000 simple memory.csv");
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "no collisions complex");
+
+		// 100000 particles - polygon1
+		floorPlanPath = "polygon1.csv";
+		initialParticleNo = 100000;
+		maxParticleNo = 100000;
+		degeneracyLimit = 50000;
+		randomFilePath = "comparisonData/bigparticlerandoms1.txt";
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "polygon1");
+
+		// wgb_second run 1 - cut number
+		floorPlanPath = "wgb_second.csv";
+		initialParticleNo = 50000;
+		maxParticleNo = 3000;
+		degeneracyLimit = 1000;
+		randomFilePath = "comparisonData/bigparticlerandoms1.txt";
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "wgb_second");
+
+		// wgb_second run 1 - constant particle no
+		floorPlanPath = "wgb_second.csv";
+		initialParticleNo = 50000;
+		maxParticleNo = 50000;
+		degeneracyLimit = 10000;
+		randomFilePath = "comparisonData/bigparticlerandoms1.txt";
+		runAndGenerateData(randomFilePath, floorPlanPath, stepVectorFilePath, initialParticleNo, maxParticleNo, degeneracyLimit, "wgb_second nocut");
+
 	}
 
 
